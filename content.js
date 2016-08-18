73020737d41de08aace416ccb534de307fbdd8c0
kimkvn <kimkvn@gmail.com>
2016-08-18 03:49:02 -0400

    FINALLY GOT THE IMAGE TO LOAD. Set the src of the element using vanilla js worked I guess

--- content.js
+++ content.js
@@ -2,11 +2,16 @@
 var box = document.getElementById('pagelet_trending_tags_and_topics');
 
 box.style.display = "none";
-var panda = chrome.extension.getURL('/images/redPanda_1.jpg');
+var panda = chrome.extension.getURL('redPanda_1.jpg');
+
 $.get(chrome.extension.getURL('/placeholder.html'), function(data) {
   $(data).appendTo('.home_right_column');
   //$(panda).appendTo('.home_right_column')
+  document.getElementById('panda').src = panda;
+
 });
+
+
 
 
 
