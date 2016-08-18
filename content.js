6f8592cce2005980364842eb62bab3f960cf5780
kimkvn <kimkvn@gmail.com>
2016-08-18 03:16:03 -0400

    trying to figure out how to insert images

--- content.js
+++ content.js
@@ -2,9 +2,13 @@
 var box = document.getElementById('pagelet_trending_tags_and_topics');
 
 box.style.display = "none";
-
+var panda = chrome.extension.getURL('/images/redPanda_1.jpg');
 $.get(chrome.extension.getURL('/placeholder.html'), function(data) {
   $(data).appendTo('.home_right_column');
+  //$(panda).appendTo('.home_right_column')
 });
+
+
+
 
 console.log('OK')
