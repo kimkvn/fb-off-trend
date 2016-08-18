2ed982a233be7aa59a13c615f866e7bf31f5f4bc
kimkvn <kimkvn@gmail.com>
2016-08-18 02:36:45 -0400

    added jquery, did some googling and found how to inject some html

--- content.js
+++ content.js
@@ -1,7 +1,10 @@
-console.log('hi')
 
 var box = document.getElementById('pagelet_trending_tags_and_topics');
 
-console.log(box)
-
 box.style.display = "none";
+
+$.get(chrome.extension.getURL('/placeholder.html'), function(data) {
+  $(data).appendTo('.home_right_column');
+});
+
+console.log('OK')
