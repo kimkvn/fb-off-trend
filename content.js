07ada29ea63bd97e745e2b09e65b540bba7acff2
kimkvn <kimkvn@gmail.com>
2016-08-18 04:26:26 -0400

    set the image urls in an array to randomize what photo will display on reload. success

--- content.js
+++ content.js
@@ -1,13 +1,31 @@
 
 var box = document.getElementById('pagelet_trending_tags_and_topics');
-
 box.style.display = "none";
-var panda = chrome.extension.getURL('redPanda_1.jpg');
+
+var photo1 = chrome.extension.getURL('images/redPanda_1.jpg');
+var photo2 = chrome.extension.getURL('images/redPanda_2.jpg');
+var photo3 = chrome.extension.getURL('images/redPanda_3.jpg');
+var photo4 = chrome.extension.getURL('images/redPanda_4.jpg');
+var photo5 = chrome.extension.getURL('images/redPanda_5.jpg');
+
+var photoSet = [
+  photo1,
+  photo2,
+  photo3,
+  photo4,
+  photo5
+];
 
 $.get(chrome.extension.getURL('/placeholder.html'), function(data) {
   $(data).appendTo('.home_right_column');
-  //$(panda).appendTo('.home_right_column')
-  document.getElementById('panda').src = panda;
+
+  var panda = document.getElementById('panda');
+  panda.style.width = "290px";
+  panda.style.padding = "15px 0 0 0";
+
+  var number = Math.floor(Math.random() * 5);
+
+  panda.src = photoSet[number];
 
 });
 
