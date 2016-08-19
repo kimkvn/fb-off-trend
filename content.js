996a2ab4eea6e2925bcc47f82bde2c4a347c5820
kimkvn <kimkvn@gmail.com>
2016-08-19 15:12:30 -0400

    trying to refactor the getURL chrome function. WE MUST GO DEEPER

--- content.js
+++ content.js
@@ -1,17 +1,28 @@
 
-var photo1 = chrome.extension.getURL('images/redPanda_1.jpg');
-var photo2 = chrome.extension.getURL('images/redPanda_2.jpg');
-var photo3 = chrome.extension.getURL('images/redPanda_3.jpg');
-var photo4 = chrome.extension.getURL('images/redPanda_4.jpg');
-var photo5 = chrome.extension.getURL('images/redPanda_5.jpg');
 
-var photoSet = [
-  photo1,
-  photo2,
-  photo3,
-  photo4,
-  photo5
-];
+var photoSet = [];
+
+photoSet.push(chrome.extension.getURL('images/redPanda_1.jpg'));
+photoSet.push(chrome.extension.getURL('images/redPanda_2.jpg'));
+photoSet.push(chrome.extension.getURL('images/redPanda_3.jpg'));
+photoSet.push(chrome.extension.getURL('images/redPanda_4.jpg'));
+photoSet.push(chrome.extension.getURL('images/redPanda_5.jpg'));
+
+
+//
+// var photoUrls = [
+//   'images/redPanda_1.jpg',
+//   'images/redPanda_2.jpg',
+//   'images/redPanda_3.jpg',
+//   'images/redPanda_4.jpg',
+//   'images/redPanda_5.jpg'
+// ];
+// var photoSet = [];
+//
+// photoUrls.forEach(function(photo, index){
+//   photoSet.push(chrome.extension.getURL(photo));
+// });
+
 
 var number = Math.floor(Math.random() * 5);
 
@@ -34,6 +45,9 @@ hideExtras();
 // };
 // appendPanda();
 
+//
+//--inserting a panda photo
+//
 function appendPanda(){
   $('<div id="pandaWrap"><img id="panda" ></div>').appendTo('.home_right_column');
   $('#panda').css('width', "280px");
@@ -41,6 +55,9 @@ function appendPanda(){
   $('#panda').attr('src', photoSet[number]);
 };
 
+//
+//--watches for partial DOM changes
+//
 $('head').bind('DOMSubtreeModified', function(){
   if($('#pandaWrap').length < 1){
     hideExtras();
