6fb1e95bc3ae52a787178db6de7eb15ac9c35c1f
kimkvn <kimkvn@gmail.com>
2016-08-24 16:34:39 -0400

    applied panda toggle to stick with DOM changes

--- content.js
+++ content.js
@@ -10,9 +10,7 @@ function showExtras(){
   document.getElementById('pagelet_ego_pane').style.display = "block";
 }
 
-//
 //--getting ready to read the images
-//
 var photoSet = [];
 
 photoSet.push(chrome.extension.getURL('images/redPanda_1.jpg'));
@@ -21,14 +19,10 @@ photoSet.push(chrome.extension.getURL('images/redPanda_3.jpg'));
 photoSet.push(chrome.extension.getURL('images/redPanda_4.jpg'));
 photoSet.push(chrome.extension.getURL('images/redPanda_5.jpg'));
 
-//
-//--Setting the RNG
-//
+//--RNG
 var number = Math.floor(Math.random() * 5);
 
-//
 //--inserting a panda photo
-//
 function appendPanda(){
   var pandaWrap = document.createElement("div");
   pandaWrap.setAttribute("class", "pandaWrap");
@@ -46,40 +40,45 @@ function appendPanda(){
 };
 appendPanda();
 
-//
-//--watches for partial DOM changes
-//
-document.head.addEventListener('DOMSubtreeModified', function(){
-  if(document.getElementsByClassName('pandaWrap').length < 1){
-    hideExtras();
-    appendPanda();
-  }
-});
+function showPanda(){
+  document.getElementById('panda').style.display = "block";
+};
+function hidePanda(){
+  document.getElementById('panda').style.display = "none";
+};
 
-// chrome.runtime.sendMessage({greeting:"hello"}, function(response){
-//   console.log(response.farewell);
-// });
+var pandaStatus;
 
 chrome.runtime.onMessage.addListener(
   function( request, sendResponse){
     if(request.status == 1){
       hideExtras();
-      document.getElementById('panda').style.display = "block";
-      console.log('yes')
-      console.log(request.status)
+      showPanda();
+      pandaStatus = 1;
+      console.log(pandaStatus)
     }
     if(request.status == 0){
       showExtras();
-      document.getElementById('panda').style.display = "none";
-      console.log('no')
-      console.log(request.status)
+      hidePanda();
+      pandaStatus = 0;
+      console.log(pandaStatus)
     }
-    // if(request.status == false){
-    //   console.log("extras are visible")
-    //   showExtras();
-    // }
-    // else if (request.status == true){
-    //   console.log("extras are hidden")
+});
+
+
+
+//--watches for partial DOM changes
+document.head.addEventListener('DOMSubtreeModified', function(){
+  if(document.getElementsByClassName('pandaWrap').length < 1){
+    hideExtras();
+    appendPanda();
+    // if (pandaStatus === 1){
     //   hideExtras();
+    //   showPanda();
+    // }else{
+    //   showExtras();
+    //   hidePanda();
     // }
+
+  }
 });
