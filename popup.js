12b0c4eb6fa00a7d87a6596a0e61d12ecfc229bd
kimkvn <kimkvn@gmail.com>
2016-08-29 17:50:31 -0400

    scratching the browser action toggle functionality for now. Dropping a link into the popup that will take users to the options page. I want to try and get this on the extension store and on my portfolio

--- popup.js
+++ popup.js
@@ -1,49 +1,7 @@
-
-$(function(){
-
-  localStorage["status"] = 1;
-
-  if(localStorage.status == 1){
-    $('#pandaBtn').addClass('offPanda');
-    $('#pandaBtn').html("I'm tired of Pandas.");
-    $('#pandaBtn').removeClass('onPanda');
-    console.log(localStorage)
-  }else if (localStorage.status == 0){
-    $('#pandaBtn').addClass('onPanda');
-    $('#pandaBtn').html('Bring back the Red Pandas!');
-    $('#pandaBtn').addClass('ofPanda');
-    console.log(localStorage)
-  }
-
-
-  $('#pandaBtn').click(function(){
-    if(localStorage.status == 1){
-      localStorage.status = 0;
-      console.log(localStorage)
-    }else{
-      localStorage.status = 1;
-      console.log(localStorage)
-    }
-
-    // if($('#pandaBtn').hasClass('onPanda')){
-    //   $('#pandaBtn').html("Bring back the Red Pandas!");
-    //   localStorage["status"] = 0;
-    //
-    // }else{
-    //   $('#pandaBtn').html("I'm tired of Red Pandas.");
-    //   localStorage["status"] = 1;
-    // }
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id, {status: localStorage.status})
+document.addEventListener('DOMContentLoaded', function(){
+  document.getElementById('options').addEventListener('click', function(){
+    chrome.tabs.update({
+      url: 'chrome://chrome/extensions'
     })
-
   });
-
-  // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-  //   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response){
-  //       console.log(response.farewell);
-  //   });
-  // });
-
-
 });
